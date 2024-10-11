692b79ee4d97d98c6ad878a7168b05caac252be9
etraunig <e.enes@edu.pucrs.br>
2024-10-11 19:03:49 -0300

    changed start scripts

--- compile_run.sh
+++ compile_run.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+
+# go where the code is
 cd ./custom-scripts/webserver/
+
+# compile
 ../../output/host/bin/i686-buildroot-linux-gnu-cc server.c -O2 -o server
 ../../output/host/bin/i686-buildroot-linux-gnu-cc app.c -O2 -o app
+
+# move compiled code to buildroot bin
 cp server app ../../output/target/usr/bin/
+
+# go back to start folder
 cd ../../
+
+# recompile qemu/buildroot
 make
+
+# run
 sudo qemu-system-i386 \
     --device e1000,netdev=eth0,mac=aa:bb:cc:dd:ee:ff \
 	--netdev tap,id=eth0,script=custom-scripts/qemu-ifup \
